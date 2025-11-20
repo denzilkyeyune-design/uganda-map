1f8eee108b7f0020f852ffc199930241f6201c85
denzilkyeyune-design <denzilkyeyune@gmail.com>
2025-11-20 16:26:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// Initialize map centered on Uganda
-var map = L.map("map").setView([1.2, 32.3], 7);
+// Initialize map
+var map = L.map('map').setView([1.3, 32.3], 7);
 
 // Basemap
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
-// Load Uganda regional boundaries
-fetch("Uganda Regional Boundaries.json")
-    .then(res => res.json())
-    .then(data => {
-        console.log("Loaded Regional Boundaries:", data);
-
-        L.geoJSON(data, {
-            style: {
-                color: "blue",
-                weight: 2,
-                fillColor: "lightblue",
-                fillOpacity: 0.2
-            },
-            onEachFeature: function (feature, layer) {
-                const region = feature.properties.ADM1_EN || "Unknown Region";
-                layer.bindPopup(`<b>${region}</b>`);
-            }
-        }).addTo(map);
-    })
-    .catch(err => console.error("FAILED TO LOAD GEOJSON:", err));
+// Load Uganda Regional Boundaries
+fetch("Uganda%20Regional%20Boundaries.json")
+  .then(res => res.json())
+  .then(data => {
+      L.geoJSON(data, {
+          style: {
+              color: "#0000ff",
+              weight: 2,
+              fillColor: "#99ccff",
+              fillOpacity: 0.4
+          },
+          onEachFeature: function (feature, layer) {
+              layer.bindPopup(`
+                  <strong>${feature.properties.ADM1_EN}</strong><br>
+                  Code: ${feature.properties.ADM1_PCODE}
+              `);
+          }
+      }).addTo(map);
+  })
+  .catch(err => console.error("Failed to load Regional Boundaries:", err));
 
 
