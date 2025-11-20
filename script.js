ce5d8749c472e6a50069a1ac0a28964bdd671877
denzilkyeyune-design <denzilkyeyune@gmail.com>
2025-11-20 17:58:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
-// Initialize map
-var map = L.map('map').setView([1.3, 32.3], 7);
+// Create map
+var map = L.map("map").setView([1.3, 32.3], 8);
 
 // Basemap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19
 }).addTo(map);
 
-// Load Uganda Regional Boundaries
-fetch("Uganda%20Regional%20Boundaries.json")
-  .then(res => res.json())
-  .then(data => {
-      L.geoJSON(data, {
-          style: {
-              color: "#0000ff",
-              weight: 2,
-              fillColor: "#99ccff",
-              fillOpacity: 0.4
-          },
-          onEachFeature: function (feature, layer) {
-              layer.bindPopup(`
-                  <strong>${feature.properties.ADM1_EN}</strong><br>
-                  Code: ${feature.properties.ADM1_PCODE}
-              `);
-          }
-      }).addTo(map);
-  })
-  .catch(err => console.error("Failed to load Regional Boundaries:", err));
+// Layer groups
+var regionsGroup = L.geoJSON(null, {
+    style: { color: "purple", weight: 2, fillOpacity: 0.1 }
+});
+
+var districtsGroup = L.geoJSON(null, {
+    style: { color: "blue", weight: 1, fillOpacity: 0.1 }
+});
+
+var villagesGroup = L.geoJSON(null, {
+    style: { color: "red", weight: 0.3, fillOpacity: 0.1 }
+});
+
+// Load Regions
+fetch("Uganda Regional Boundaries.json")
+    .then(res => res.json())
+    .then(data => {
+        regionsGroup.addData(data);
+        console.log("Regions loaded");
+    });
+
+// Load Districts
+fetch("Uganda District Boundaries 2014.geojson")
+    .then(res => res.json())
+    .then(data => {
+        districtsGroup.addData(data);
+        console.log("Districts loaded");
+    });
+
+// Load Villages
+fetch("Uganda Villages 2009.json")
+    .then(res => res.json())
+    .then(data => {
+        villagesGroup.addData(data);
+        console.log("Villages loaded");
+    });
+
+// Toggle Layers
+document.getElementById("regionsLayer").addEventListener("change", function () {
+    if (this.checked) map.addLayer(regionsGroup);
+    else map.removeLayer(regionsGroup);
+});
+
+document.getElementById("districtsLayer").addEventListener("change", function () {
+    if (this.checked) map.addLayer(districtsGroup);
+    else map.removeLayer(districtsGroup);
+});
+
+document.getElementById("villagesLayer").addEventListener("change", function () {
+    if (this.checked) map.addLayer(villagesGroup);
+    else map.removeLayer(villagesGroup);
+});
 
 
